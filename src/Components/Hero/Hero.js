3bb54941ab2bc6fcfde30e359f7b12101da952dd
kelvin123204 <kelvin123204@gmail.com>
2023-05-07 03:56:20 +0100

    update Hero to use css module

--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -1,22 +1,22 @@
 import React from "react";
 import img from "../../asset/restauranfood.jpg";
-import "./Hero.css";
+import classes from "./Hero.module.css";
 
 const Hero = () => {
   return (
     <React.Fragment>
-      <div className="container">
-        <div className="left-container">
-          <h1 className="title">Little Lemon</h1>
-          <h3 className="location">Chicago</h3>
-          <p className="desc">
+      <div className={classes.container}>
+        <div className={classes.leftContainer}>
+          <h1 className={classes.title}>Little Lemon</h1>
+          <h3 className={classes.location}>Chicago</h3>
+          <p className={classes.desc}>
             Ullamco deserunt amet esse irure officia id dolore qui fugiat
             exercitation nulla consequat elit consequat. Proident exercitation
             fugiat Lorem veniam fugiat pariatur nisi veniam sint.
           </p>
-          <button className="button">Reserve a Table</button>
+          <button className={classes.button}>Reserve a Table</button>
         </div>
-        <img className="img" src={img} alt="resturant food" />
+        <img className={classes.img} src={img} alt="resturant food" />
       </div>
     </React.Fragment>
   );
